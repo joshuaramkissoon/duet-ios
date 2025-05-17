0ba237cb99f6ee3a17aa7a7f59fd070bee88d152
Joshua Ramkissoon <joshuaramkissoon@hotmail.com>
2025-05-17 02:30:35 +0100

    Initial Commit

---
+++ Duet/DuetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuetApp.swift
+//  Duet
+//
+//  Created by Joshua Ramkissoon on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DuetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DuetTests/DuetTests.swift
@@ -0,0 +1,16 @@
+//
+//  DuetTests.swift
+//  DuetTests
+//
+//  Created by Joshua Ramkissoon on 17/05/2025.
+//
+
+import Testing
+
+struct DuetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
